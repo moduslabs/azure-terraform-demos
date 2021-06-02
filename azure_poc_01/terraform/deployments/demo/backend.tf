dc41061afbef36c8f2ecfc6371b7178e7036fd46
Fernando Matsuo Santos <fernando.matsuo@moduscreate.com>
2021-06-02 16:44:19 -0300

    Azure PoC - initial commit

---
+++ azure_poc_01/terraform/deployments/demo/backend.tf
@@ -0,0 +1,17 @@
+# https://github.com/hashicorp/terraform/releases
+# https://github.com/terraform-providers/terraform-provider-azurerm/releases
+terraform {
+  required_version = "0.15.4"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "2.60.0"
+    }
+  }
+  backend "azurerm" {
+    resource_group_name  = "poc-terraform-tfstate"
+    storage_account_name = "pocfmsterraform"
+    container_name       = "tfstate"
+    key                  = "dev.terraform.tfstate"
+  }
+}
